2397fe2ba91499ed10042824392c8e798b492883
Zhoulu <wangzhoulu1998@163.com>
2023-04-25 00:53:16 +0800

    添加了vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     
     <router-view/>
+    <h1>我是黑马头条</h1>
+    <p>
+      <i class="iconfont icon-yanzhengma"></i>
+    </p>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import store from './store'
 //引入全局样式
 import './styles/index.less'
 
+//引入vant全部组件
+import Vant from 'vant'
+
+//引入vant样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
